9ff2570e9f41638f40d5b1ce2699da5b94c3ad82
Leticia C. Junqueira <67600443+itsmejunqueira@users.noreply.github.com>
2020-10-15 19:19:58 -0300

    Projeto finalizado

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slideToShow: 1,
+    adaptivwHeigth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
